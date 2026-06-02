adf4ddccf427a075de38df89f4eae53032019e99
TechSolanki <mradulpratapsolanki@gmail.com>
2026-06-02 17:40:19 +0530

    Update plugin.js

--- Pervzila/plugin.js
+++ Pervzila/plugin.js
@@ -1,4 +1,4 @@
-    (function () {
+(function () {
     const HEADERS = {
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36")",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
